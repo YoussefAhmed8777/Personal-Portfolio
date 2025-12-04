06d75c769f5aeefe5ad23f06dda823487ee8b715
YoussefAhmed8777 <youssefahmedamin28@gmail.com>
2025-12-04 20:28:31 +0200

    edited: navbar & footer styles

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -10,7 +10,14 @@ export class Navbar {
 
   toggleMenu(){
     let menu = document.getElementById('content');
+    let menuBtnIcon = document.getElementById('menuBtnIcon');
     menu?.classList.toggle('hidden');
+
+    if (!menu?.classList.contains('hidden')) {
+      menuBtnIcon?.classList.replace('fa-bars','fa-xmark');
+    }else{
+      menuBtnIcon?.classList.replace('fa-xmark','fa-bars');
+    };
   };
 
 }
